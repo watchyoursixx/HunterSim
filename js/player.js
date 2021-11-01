b64088ff2ac49a9feb5a987578ed9424d508c50b
watchyoursixx <47162044+watchyoursixx@users.noreply.github.com>
2021-10-31 20:54:26 -0400

    Update player.js
    setting up player stats in a easy to follow way - maybe simplify with for's later?

--- js/player.js
+++ js/player.js
@@ -1,3 +1,9 @@
+const HitRatingRatio = 15.77;
+const CritRatingRatio = 22.08;
+const HasteRatingRatio = 15.77;
+const AgiToCrit = 1/40;
+const BaseCritChance = -1.53;
+
 // initialize base stats
 var BaseRAP = 0;
 var RangedCritRating = 0;
@@ -25,11 +31,13 @@ var intmod = 1;
 var spimod = 1;
 var mapmod = 1;
 var rapmod = 1;
+var dmgmod = 1;
 var rangedmgmod = 1;
 
 // temp settings to test with
 var CarefulAimTalent = (0 * 0.15);
 var selectedRace = 3; // 0 for night elf, 1 for dwarf, 2 for draenei, 3 for orc, 4 for troll, 5 for tauren, 6 for blood elf -- temp?
+var equippedRangeType = 1; // 0 for gun, 1 for bow, 2 for crossbow
 // included here to test usage from same file
 var races = [
    {
@@ -41,6 +49,7 @@ var races = [
       spi: 83,
       mAP: 120,
       rAP: 130,
+      critchance: 0,
    },{
       name: 'Dwarf',
       str: 66,
@@ -50,6 +59,7 @@ var races = [
       spi: 82,
       mAP: 120,
       rAP: 130,
+      critchance: 0,
    },{
       name: 'Draenei',
       str: 65,
@@ -59,6 +69,7 @@ var races = [
       spi: 85,
       mAP: 120,
       rAP: 130,
+      critchance: 0,
    },{
       name: 'Orc',
       str: 67,
@@ -68,6 +79,7 @@ var races = [
       spi: 86,
       mAP: 120,
       rAP: 130,
+      critchance: 0,
    },{
       name: 'Troll',
       str: 65,
@@ -77,6 +89,7 @@ var races = [
       spi: 84,
       mAP: 120,
       rAP: 130,
+      critchance: 0,
    },{
       name: 'Tauren',
       str: 69,
@@ -86,6 +99,7 @@ var races = [
       spi: 85,
       mAP: 120,
       rAP: 130,
+      critchance: 0,
    },{
       name: 'Blood Elf',
       str: 61,
@@ -95,22 +109,37 @@ var races = [
       spi: 82,
       mAP: 120,
       rAP: 130,
+      critchance: 0,
    },
 ];
-
-// gear and buff objects to sum out of combat gear and buff stats
+function checkWeaponType(){
+   // check for gun and dwarf, or bow and troll
+   if ((equippedRangeType === 0 && selectedRace === 1) || (equippedRangeType === 1 && selectedRace === 4)) {
+      races[selectedRace].critchance = 1;
+   } else {
+      races[selectedRace].critchance = 0;
+   }
+}
+      
+// gear, buff, talent objects to sum out of combat gear and buff stats
 var GearStats = {Str:0, Agi:0, Stam:0, Int:0, Spi:0, RAP:0, MAP:0, Crit:0, Hit:0, MP5:0, Resil:0, ArP:0, Haste:0}; 
-var BuffStats = {Str:0, Agi:0, Stam:0, Int:0, Spi:0, RAP:155, MAP:0, Crit:0, Hit:0, MP5:0, Resil:0, ArP:0, Haste:0}; // added 155 for hawk for now
+var BuffStats = {Str:0, Agi:0, Stam:0, Int:0, Spi:0, RAP:155, MAP:0, Crit:0, CritChance:0, Hit:0, HitChance:0, MP5:0, Resil:0, ArP:0, Haste:0}; // added 155 for hawk for now
+var TalentStats = {CritChance:0, RangeCritChance:0, HitChance:0, agimod:1, mapmod:1, rapmod:1, intmod:1, dmgmod:1, rangedmgmod:1};
 
 // test function for initializing base stats
 function calcBaseStats() {
-  
+  // Main Stats
   Str = Math.floor((GearStats.Str + BuffStats.Str + races[selectedRace].str) * strmod);
   Agi = Math.floor((GearStats.Agi + BuffStats.Agi + races[selectedRace].agi) * agimod);
   Stam = Math.floor((GearStats.Stam + BuffStats.Stam + races[selectedRace].stam) * stammod);
   Int = Math.floor((GearStats.Int + BuffStats.Int + races[selectedRace].int) * intmod);
   Spi = Math.floor((GearStats.Spi + BuffStats.Spi + races[selectedRace].spi) * spimod);
-  
+  // Attack Power
   BaseMAP = (GearStats.MAP + BuffStats.MAP + Agi + Str + races[selectedRace].mAP) * mapmod;
   BaseRAP = (GearStats.RAP + BuffStats.RAP + Agi + races[selectedRace].rAP + Int * (CarefulAimTalent)) * rapmod;
+  // Crit rating and crit chance
+  MeleeCritRating = GearStats.Crit + BuffStats.Crit;
+  RangeCritRating = GearStats.Crit + BuffStats.Crit;
+  MeleeCritChance = BaseCritChance + Agi / AgiToCrit + MeleeCritRating / CritRatingRatio + BuffStats.CritChance;
+  RangeCritChance = BaseCritChance + Agi / AgiToCrit + RangeCritRating / CritRatingRatio + BuffStats.CritChance + TalentStats.RangedCritChance;
 }
