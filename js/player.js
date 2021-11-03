54329e2326a63f75ee8c23c7fc0c1188a849a8fe
watchyoursixx <47162044+watchyoursixx@users.noreply.github.com>
2021-11-03 03:48:42 -0400

    Update player.js
    adding more testing, playing around with formatting and exploring how to connect the different stats

--- js/player.js
+++ js/player.js
@@ -51,9 +51,10 @@ var rangedmgmod = 1;
 // temp settings to test with
 var CarefulAimTalent = (0 * 0.15);
 var selectedRace = 0; // 0 for night elf, 1 for dwarf, 2 for draenei, 3 for orc, 4 for troll, 5 for tauren, 6 for blood elf -- temp? 0 for orc now, simplified race for testing
-var equippedRanged = 15808;
-var equippedMainhand = 28587;
 var offhandDisabled = false;
+
+// initial variables for item id's (like a profile)
+const gear = {head:0,neck:0,shoulder:0,back:0,chest:0,wrist:0,hand:0,waist:0,leg:0,feet:0,ring1:0,ring2:0,trinket1:0,trinket2:0,mainhand:28587,offhand:0,range:15808};
 
 // included here to test usage from same set of code -- will remove later
 var races = [
@@ -71,19 +72,19 @@ var races = [
       expertise: 0,
    },
 ];
-// adding in test rangeweps
-var rangeweps = {
+// adding in test rangelist
+const range = {
     15808:  {
                 name: 'Fine Light Crossbow',
                 mindmg: 29,
                 maxdmg: 29,
                 speed: 2.7,
-                type: 2,
+                type: 'crossbow',
                 itemid: 15808,
     }
 };
-// adding in test mainhandweps
-var mainhandweps = {
+// adding in test mainhandlist
+const mainhand = {
     28587:  {
                 name: 'Legacy',
                 Agi: 40,
@@ -95,42 +96,112 @@ var mainhandweps = {
                 maxdmg: 479,
                 speed: 3.5,
                 itemid: 28587,
-                type: 3,
-                hand: 3,
+                type: 'axe',
+                hand: 'two',
     }
 };
 
-var equippedRangeType = rangeweps[equippedRanged].type; // 0 for gun, 1 for bow, 2 for crossbow
-var equippedMHType = mainhandweps[equippedMainhand].type;
-
 function checkWeaponType(){
+
+   let equippedRangeType = range[gear.range].type; 
+   let equippedMHType = mainhand[gear.mainhand].type;
    // check for gun and dwarf, or bow and troll
-   if ((equippedRangeType === 0 && selectedRace === 1) || (equippedRangeType === 1 && selectedRace === 4)) {
+   if ((equippedRangeType === 'gun' && selectedRace === 1) || (equippedRangeType === 'bow' && selectedRace === 4)) {
       races[selectedRace].critchance = 1;
    } else {
       races[selectedRace].critchance = 0;
    }
    // check for axe and orc
-   if (equippedMHType === 3 && selectedRace === 0) {
+   if (equippedMHType === 'axe' && selectedRace === 0) {
       races[selectedRace].expertise = 5;
    } else {
       races[selectedRace].expertise = 0;
    }
    // hand options: 0 for main-hand, 1 for one-hand, 2 for off-hand, 3 for two-hand
-   offhandDisabled = (mainhandweps[equippedMainhand].hand === 3) ? true:false; 
+   offhandDisabled = (mainhand[gear.mainhand].hand === 'two') ? true:false; 
 }
       
 // gear, buff, talent objects to sum out of combat gear and buff stats
-var GearStats = {Str:0, Agi:0, Stam:0, Int:0, Spi:0, RAP:0, MAP:0, Crit:0, Hit:0, MP5:0, Resil:0, ArP:0, Haste:0, ExpRating:0}; 
+var GearStats = {Str:0, Agi:0, Stam:0, Int:0, Spi:0, RAP:0, MAP:0, Crit:0, Hit:0, MP5:0, Resil:0, ArP:0, Haste:0, Exp:0}; 
+
 function addGear(){
-    for(let prop in GearStats) {
-        GearStats[prop] += mainhandweps[equippedMainhand][prop] || 0;
-    }
+      for(let prop in GearStats) {
+
+        GearStats[prop] += range[gear.range][prop] || 0;
+        GearStats[prop] += mainhand[gear.mainhand][prop] || 0;
+      
+      }
 }
 
 var BuffStats = {Str:0, Agi:0, Stam:0, Int:0, Spi:0, RAP:155, MAP:0, Crit:0, CritChance:0, Hit:0, HitChance:0, MP5:0, Resil:0, ArP:0, Haste:0}; // added 155 for hawk for now
 var TalentStats = {CritChance:0, RangeCritChance:0, HitChance:0, agimod:1, mapmod:1, rapmod:1, intmod:1, dmgmod:1, rangedmgmod:1};
 var EnchantStats = {Str:0, Agi:0, Stam:0, Int:0, Spi:0, RAP:0, MAP:0, Crit:0, RangeCrit:0, Hit:0, RangeHit:0, MP5:0, Resil:0, ArP:0, Haste:0, dmgbonus:0, rangedmgbonus:0}; 
+var talents = {
+   imp_hawk: 1.15,
+   end_training: 1.01,
+   focused_fire: 2,
+   imp_monkey: 0,
+   thick_hide: 0,
+   imp_ress_pet: 2,
+   pathfinding: 0,
+   bestial_swift: 0,
+   unleash_fury: 1.2,
+   imp_mend_pet: 2,
+   ferocity: 10,
+   spirit_bond: 0,
+   intimidation: 1,
+   bestial_disc: 2,
+   animal_handler: 4,
+   frenzy: 4,
+   ferocious_insp: 1.03,
+   bestial_wrath: 1,
+   catlike_reflex: 0,
+   serp_swift: 1.2,
+   beast_within: 1,
+   imp_conc_shot: 0,
+   lethal_shots: 5,
+   imp_hunter_mark: 0,
+   efficiency: 0.9,
+   GftT: 50,
+   imp_arc_shot: 0,
+   aimed_shot: 1,
+   rapid_killing: 2,
+   imp_stings: 1,
+   mortal_shots: 1.3,
+   conc_barrage: 0,
+   scatter_shot: 0,
+   barrage: 1,
+   combat_exp: 0,
+   ranged_weap_spec: 1,
+   careful_aim: 0,
+   trueshot_aura: 0,
+   imp_barrage: 0,
+   master_marksman: 1,
+   silencing_shot: 0,
+   monster_slaying: 1,
+   humanoid_slaying: 1,
+   hawk_eye: 0,
+   savage_strikes: 0,
+   entrapment: 0,
+   deflection: 0,
+   imp_wing_clip: 0,
+   clever_traps: 1,
+   survivalist: 1,
+   deterrence: 0,
+   trap_mastery: 0,
+   surefooted: 0,
+   imp_fd: 0,
+   surv_instincts: 1,
+   killer_instinct: 0,
+   counterattack: 0,
+   resourcefulness: 0,
+   light_reflexes: 1,
+   TotH: 0,
+   wyvern_sting: 0,
+   exp_weakness: 0,
+   master_tac: 0,
+   readiness: 0
+ }
 
 // test function for initializing base stats
 function calcBaseStats() {
@@ -144,17 +215,17 @@ function calcBaseStats() {
    
   // Attack Power
   BaseMAP = (GearStats.MAP + BuffStats.MAP + EnchantStats.MAP + Agi + Str + races[selectedRace].mAP) * mapmod;
-  BaseRAP = (GearStats.RAP + BuffStats.RAP + EnchantStats.RAP + Agi + races[selectedRace].rAP + Int * (CarefulAimTalent)) * rapmod;
+  BaseRAP = (GearStats.RAP + BuffStats.RAP + EnchantStats.RAP + Agi + races[selectedRace].rAP + Int * talents.careful_aim) * rapmod;
    
   // Crit rating and crit chance
    let critrating = GearStats.Crit + BuffStats.Crit + EnchantStats.Crit;
   MeleeCritRating = critrating;
   RangeCritRating = critrating + EnchantStats.RangeCrit;
-   let crit = BaseCritChance + Agi / AgiToCrit + races[selectedRace].critchance + BuffStats.CritChance + TalentStats.CritChance;
+   let crit = BaseCritChance + Agi / AgiToCrit + BuffStats.CritChance + TalentStats.CritChance;
   MeleeCritChance = crit + MeleeCritRating / CritRatingRatio;
-  RangeCritChance = crit + RangeCritRating / CritRatingRatio + TalentStats.RangeCritChance;
+  RangeCritChance = crit + RangeCritRating / CritRatingRatio + TalentStats.RangeCritChance + races[selectedRace].critchance;
    
-  // Hit rating and hit chance
+  // Hit rating and hit chance - split between ranged and melee because of hit scope and crit scope and racial
    let hitrating = GearStats.Hit + BuffStats.Hit + EnchantStats.Hit;
   MeleeHitRating = hitrating;
   RangeHitRating = hitrating + EnchantStats.RangeHit;
@@ -166,7 +237,7 @@ function calcBaseStats() {
   RangeMissChance = 8 - RangeHitChance - penalty;
    
   // Expertise and Dodge - every 3.9 rating is 1 expertise, 1 expertise = 0.25% reduction rounded down to nearest integer
-  Expertise = Math.floor(GearStats.ExpRating / ExpertiseRatio + races[selectedRace].expertise);
+  Expertise = Math.floor(GearStats.Exp / ExpertiseRatio + races[selectedRace].expertise);
   DodgeChance = 6.5 - Expertise * 0.25;
   
 }
